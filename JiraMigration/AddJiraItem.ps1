892afb589f5ad6b77d632df1cb08677263a12062
piern68 <pierluigi.nanni@prometeia.com>
2020-12-01 18:35:57 +0100

    Create AddJiraItem.ps1

---
+++ JiraMigration/AddJiraItem.ps1
@@ -0,0 +1,31 @@
+[string] $target = "https://prometeia-erm.atlassian.net"
+[string] $Username = "pierluigi.nanni@prometeia.com"
+[string] $token = 'WhkVRH0WhMOhbehcLYxg3332'
+[string] $projectKey = "SKP"
+[string] $issueType = "Story"
+[string] $summary = "Api call"
+[string] $description = "Testing api call"
+
+
+
+[string] $body = "{`"fields`":{`"project`":{`"key`":`"$projectKey`"},`"issuetype`":{`"name`":`"$issueType`"},`"summary`":`"$summary`",`"description`":`"$description`"}}";
+
+try 
+    {
+        $basicAuth = "Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($Username):$token"))
+        #$basicAuth = "Bearer $token"
+        $headers = @{
+        "Authorization" = $basicAuth
+        "Content-Type" = "application/json"
+        "verify" = "false"
+    }
+    $requestUri = "$target/rest/api/latest/issue"
+    $response = Invoke-RestMethod -Uri $requestUri -Method POST -Headers $headers -Body $body
+    Write-Output "ID: $($response.id)"
+    Write-Output "Key: $($response.key)"
+    Write-Output "Self: $($response.self)" 
+}
+catch {
+    Write-Warning "Remote Server Response: $($_.Exception.Message)"
+    Write-Output "Status Code: $($_.Exception.Response.StatusCode)"
+}
