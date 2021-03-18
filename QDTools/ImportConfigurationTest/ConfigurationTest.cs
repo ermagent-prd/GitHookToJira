f2264b67b0d8d1734f8181bbb9cfd244f1f37dba
lucap <paolo.luca@prometeia.com>
2021-03-18 19:11:01 +0100

    test config update

--- QDTools/ImportConfigurationTest/ConfigurationTest.cs
+++ QDTools/ImportConfigurationTest/ConfigurationTest.cs
@@ -102,12 +102,183 @@ namespace ImportConfigurationTest
                     SubTaskTypeCode = "10003",
                     UatTypeCode = "10014",
                     BugTypeCode = "10004",
-                }
+                },
+
+                Mapping = new MappingConfiguration()
+                {
+                    AFFECTEDBUILD_LABEL = "AffectedBuild",
+                    RELEASE_KEY_LABEL = "Release Version",
+                    LINE_KEY_LABEL = "DVL",
+                    
+                    FUNCTIONALITY_LABEL = "Functionality",
+                    RELATED_DEVELOPMENT_LABEL = "Development",
+                    ISSUE_TYPE_LABEL = "IssueType",
+                    FIXED_IN_BUILD_LABEL = "FixedInBuild",
+
+                    BUG_AFFECTEDBUILD_LABEL = "FoundInBuild",
+                    BUG_PROJECT_MODULE = "Product Module",
+
+                    DEV_STATUS_MAPPING = new Dictionary<string, string>()
+                    {
+                        { "backlog",        "Backlog" },
+                        { "in Backlog",     "Backlog" },
+                        { "assigned",       "Select for development" },
+                        { "analysis",       "In Progress" },
+                        { "development",    "In progress" },
+                        { "waiting for test", "In progress" },
+                        { "testing", "In progress" },
+                        { "cancelled", "Done" },
+                        { "done", "Done" },
+                        { "in progress", "In progress" },
+                    },
+                    DEV_STATUS_MAPPING_DEFAULT = "Backlog",
+
+                    DEV_ESTIMATE_TYPE_MAPPING = new Dictionary<string, string>()
+                    {
+                        { "planned", "Planned" },
+                        { "cr-mkt", "Internal change request" },
+                        { "cr-int",  "Market change request" },
+                        { "overbudget", "Overbudget" },
+                    },
+
+                    DEV_ESTIMATE_TYPE_MAPPING_DEFAULT = "Planned",
+
+
+                    TASK_TYPE_MAPPING = new Dictionary<string, string>()
+                    {
+                        { "analysis", "Analysis" },
+                        { "development", "Development" },
+                        { "test",       "Test" },
+                        { "documentation",    "Documentation" },
+                    },
+                    TASK_TYPE_MAPPING_DEFAULT = "Other",
+
+                    UAT_STATUS_MAPPING = new Dictionary<string, string>()
+                    {
+                        { "assigned",   "To Do" },
+                        { "in progress",   "In progress" },
+                        { "fixed",  "Ready for test" },
+                        { "testing",   "Testing" },
+                        { "closed",  "Closed" },
+                        { "rejected",   "Rejected" },
+                        { "cancelled",   "Cancelled" },
+                        { "in Backlog",  "In Backlog" },
+                    },
+                    UAT_STATUS_MAPPING_DEFAULT = "To Do",
+
+                    UAT_PRIORITY_MAPPING = new Dictionary<string, string>()
+                    {
+                        { "low", "Low" },
+                        { "medium", "Medium" },
+                        { "high", "High" },
+                    },
+
+                    UAT_SEVERITY_MAPPING = new Dictionary<string, string>()
+                    {
+                        { "low", "Trivial" },
+                        { "medium", "Minor" },
+                        { "high", "Major" },
+                        { "highest", "Blocking" },
+                    },
+
+                    UAT_CATEGORY_MAPPING = new Dictionary<string, string>()
+                    {
+                        {"calcoli", "Functional"},
+                        {"interfaccia", "Usability"},
+                        {"usabilità", "Usability"},
+                        {"efficenza", "Performance"},
+                        {"suggerimento", "Usability"},
+                        {"localizzazione", "Usability"},
+                        {"messaggio di errore", "Functional"},
+                        {"dal", "Functional"}
+                    },
+                    UAT_CATEGORY_MAPPING_DEFAULT = "Functional",
+
+
+                    UAT_TYPE_MAPPING = new Dictionary<string, string>()
+                    {
+                        { "defect",   "Defect" },
+                        { "investigation",   "Investigation" },
+                        { "enhancement",   "Enhancement" },
+                        { "enanchement",   "Enhancement" },
+                        { "regression",   "Regression" },
+                        { "setup",   "Setup" },
+                        { "change request",   "Change request" },
+                        { "new feature",   "New Feature" },
+                        { "missing functionality",   "Missing Functionality" },
+                    },
+
+                    BUG_STATUS_MAPPING = new Dictionary<string, string>()
+                    {
+                        { "backlog", "Backlog" },
+                        { "assigned", "Backlog" },
+                        { "in progess", "Backlog" },
+                        { "won’t fix", "Backlog" },
+                        { "not a Bug", "Backlog" },
+                        { "fixed", "Backlog" }
+                    },
+
+                    BUG_PRIORITY_MAPPING = new Dictionary<string, string>()
+                    {
+                        { "low", "Low" },
+                        { "medium", "Medium" },
+                        { "high", "High" },
+                    },
+
+                    BUG_TYPE_MAPPING = new Dictionary<string, string>()
+                    {
+                        { "Presentation", "Functional" },
+                        { "Engine", "Functional" },
+                        { "Sythesis", "Functional" },
+                        { "Other", "Functional" },
+                    },
+
+                    BUG_CAUSE_TYPE_MAPPING = new Dictionary<string, string>()
+                    {
+                        {"development mistake","Development mistake" },
+                        {"analysis mistake","Analysis mistake" },
+                        {"sql mistake","Development mistake" },
+                        {"presentation mistake","Presentation mistake" },
+                        {"enhancement","Enhancement" },
+                        {"missing development","Missing development" },
+                        {"regression","Regression" },
+                        {"refactoring","Refactoring" },
+                        {"licensing","Licensing" },
+                        {"complexity","Code complexity" },
+                        {"db upgrade","Datasource upgrade" },
+                        {"devops","Regression" },
+                        {"duplicate code","Development mistake" },
+                        {"performance","Performance" }
+                    },
+
+                    BUG_CAUSE_TYPE_DEFAULT = "Other",
+
+                    BUG_SUGGESTED_ACTION_TYPE_MAPPING = new Dictionary<string, string>()
+                    {
+                        {"more attention", "Integration test" },
+                        {"more test", "Integration test" },
+                        {"unit test", "Test automation" },
+                        {"tca", "Integration test" },
+                        {"refactoring", "Refactoring" },
+                        {"presentation test", "Integration test" },
+                        {"missing documentation", "Documentation" },
+                        {"uat", "Integration test" },
+                        {"database test", "Integration test" },
+                        {"enancement management", "Processes improvement" },
+                        {"performance test", "Performance test" },
+                        {"eagle Integration", "Integration test" },
+                        {"licence test", "Licence test" },
+                        {"use entity framework", "Refactoring" },
+                        {"sql test", "Integration test" }
+                    },
+
+                    BUG_SUGGESTED_ACTION_TYPE_DEFAULT = "Other",
+    }
             };
 
             // serialize JSON to a string and then write string to a file
             File.WriteAllText(@"C:\GeminiPorting\configuration.json", JsonConvert.SerializeObject(configuration, Formatting.Indented));
-            
+
         }
 
         [TestMethod]
