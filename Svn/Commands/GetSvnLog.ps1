cde555ffcb7ce35e9d0f45d576facc0a85ef46b7
piern68 <pierluigi.nanni@prometeia.com>
2022-06-21 16:33:47 +0200

    snv to excel

--- Svn/Commands/GetSvnLog.ps1
+++ Svn/Commands/GetSvnLog.ps1
@@ -1,28 +1,35 @@
 param (
 [string]$SendEmailsTo = "pierluigi.nanni@prometeia.com",
 [string]$SVNUser = "piern",
-[string]$SVNPass = "******",
 [string]$SVNbranchRoot = "http://erm-codever-p01.prometeia.lan/svn/ErmasNet/branches/releases/5.30.0",
 [string]$SvnPath = "C:\Program Files\TortoiseSVN\bin\svn.exe",
 [string]$ExcelFilePath = "C:\Tmp\Ermas530Logs.xlsx",
 [string]$sheetName = "Ermas530Commit",
 [string]$revisionFrom = "157734",
-[string]$revisionTo = "157808"
+[string]$revisionTo = "HEAD"
 )
 
 Function GetSVNLogs($revRange)
 {
 
-([xml](&$SvnPath log $SVNbranchRoot -r $revRange --limit 500 --xml --username $SVNUser --password $SVNPass)).log.logentry | % {
+$securePass = Read-Host "What is your svn password for user ${SVNUser} ?" -AsSecureString
+
+$pass = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+    [Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePass))
+
+$jiraRegex = "JIRA:\w+-\d+"
+
+([xml](&$SvnPath log $SVNbranchRoot -r $revRange --limit 500 --xml --username $SVNUser --password $pass)).log.logentry | % {
     $entry = $_;
     $_.paths.path | Foreach-Object {
-      $obj = 1 | Select-Object -Property Revision,Author,Date,Message,Action,FilePath;
+      $obj = 1 | Select-Object -Property Revision,Author,Date,Message,Action,FilePath,JiraRef;
       $obj.Revision = [int]$entry.Revision;
       $obj.Author = $entry.Author;
       $obj.Date = $entry.Date;
       $obj.Message = $entry.msg;
       $obj.Action = $_.action;
       $obj.FilePath = $_.InnerText;
+      $obj.JiraRef = [regex]::matches($_.InnerText, $jiraRegex).Value
       return $obj;
     }
   }
