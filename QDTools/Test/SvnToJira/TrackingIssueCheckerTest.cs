cba30fb61eeb636aed2bf30735670a921dec751d
samuelban <109357426+samuelban@users.noreply.github.com>
2022-08-24 15:53:54 +0200

    Create TrackingIssueCheckerTest.cs

---
+++ QDTools/Test/SvnToJira/TrackingIssueCheckerTest.cs
@@ -0,0 +1,35 @@
+﻿using JiraTools.Engine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SvnToJira.Engine;
+using SvnToJira.Parameters;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TrackingIssueCheckerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var checkedBranches = new List<ReleasesBranchInfo>();
+
+            checkedBranches.Add(
+                new ReleasesBranchInfo()
+                {
+                    Path = "/branches/releases/5.30.0",
+                    ReleaseName = "Ermas 5.30.0"
+                }
+            );
+
+            string trackingIssue = "ERMAS-22694";
+
+            //var engine = new TrackingIssueChecker();
+
+            //var actual = engine.Execute(trackingIssue, checkedBranches);
+
+
+        }
+    }
+}
